785547837a435a72717cea48ad8d584f4d0ac66f
keli.genius <keli.genius@gmail.com>
2017-03-15 23:04:29 +0000

    add uf1 to evaluations function

--- testproblem/evaluation.c
+++ testproblem/evaluation.c
@@ -25,7 +25,7 @@
 # include "../header/problems.h"
 # include "../header/print.h"
 
-void evaluate_population (population_real* pop)
+void evaluate_population (population_real *pop)
 {
     int i;
 
@@ -35,7 +35,7 @@ void evaluate_population (population_real* pop)
     return;
 }
 
-void evaluate_individual (individual_real* ind)
+void evaluate_individual (individual_real *ind)
 {
     int flag;
 
@@ -52,6 +52,7 @@ void evaluate_individual (individual_real* ind)
     (strcmp (problem_name, "DTLZ5") != 0)? :(dtlz5 (ind->xreal, ind->obj), flag =1);
     (strcmp (problem_name, "DTLZ6") != 0)? :(dtlz6 (ind->xreal, ind->obj), flag =1);
     (strcmp (problem_name, "DTLZ7") != 0)? :(dtlz7 (ind->xreal, ind->obj), flag =1);
+    (strcmp (problem_name, "UF1") != 0)? :(uf1 (ind->xreal, ind->obj), flag =1);
 
     print_error (flag == 0, 2, "UNKNOWN test problem: ", problem_name);
 
