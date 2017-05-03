3d1f94740251446482ea5eb5607877ab0028aac1
Renzhi Chen <chenrenzhi1989@gmail.com>
2017-05-03 20:05:02 +0100

    modified:   header/metaheuristics.h

--- header/metaheuristics.h
+++ header/metaheuristics.h
@@ -45,6 +45,7 @@ void MOEAD_STM_DRA (population_real* parent_pop, population_real* offspring_pop,
 void SMSEMOA (population_real* parent_pop, population_real* offspring_pop, population_real* mixed_pop);
 void IBEA (population_real* parent_pop, population_real* offspring_pop, population_real* mixed_pop);
 void HypE (population_real* parent_pop, population_real* offspring_pop, population_real* mixed_pop);
+void NSGA3 (population_real* parent_pop, population_real* offspring_pop, population_real* mixed_pop);
 
 /* external variables used by MOEA/D-STM variants */
 extern int *idx ;
